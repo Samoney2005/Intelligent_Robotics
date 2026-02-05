8db2cea869d5137bd414523896d5f398949dfebc
Samone Cook <samone.a.cook-1@ou.edu>
2026-02-04 19:25:38 -0600

    Add MinimalParam node with parameter handling

---
+++ Homeworks/HW1-intparameter.cpp
@@ -0,0 +1,36 @@
+#include <chrono>
+#include <functional>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
+
+class MinimalParam : public rclcpp::Node
+{
+public:
+  MinimalParam()
+  : Node("minimal_param_node")
+  {
+    this->declare_parameter("my_parameter", "world");
+    timer_ = this->create_wall_timer(
+      1000ms, std::bind(&MinimalParam::timer_callback, this));
+  }
+
+  void timer_callback()
+  {
+    std::string my_param = this->get_parameter("my_parameter").as_string();
+    RCLCPP_INFO(this->get_logger(), "Hello %s!", my_param.c_str());
+  }
+
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalParam>());
+  rclcpp::shutdown();
+  return 0;
+}
